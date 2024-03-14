8ebe06270c04e2416cfdf33ee0ba30a90d3ec240
naptawla55 <youssefirmani16@gmail.com>
2024-03-14 22:35:19 +0100

    Mainco-Finished

--- Site/js/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.getElementById('login').addEventListener('click', function() {
-    alert('Login functionality goes here.');
-  });
-  
-  document.getElementById('play-button').addEventListener('click', function() {
-    alert('Play game functionality goes here.');
-  });
-  
